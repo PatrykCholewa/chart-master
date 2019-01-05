4a70cb09ee65d0e879f912f05595b6d5db79dd17
PAwelZ97 <pawelzych97@gmail.com>
2019-01-05 23:05:06 +0100

    first messy

--- src/components/ExampleDataSheet.js
+++ src/components/ExampleDataSheet.js
@@ -3,28 +3,69 @@ import React from "react";
 import ReactDataSheet from 'react-datasheet';
 
 class ExampleDataSheet extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
-            grid: [
-                [{value:  1}, {value:  3}],
-                [{value:  2}, {value:  4}]
-            ]
+            x: 2,
+            y: 2,
+            dataXY: [['4','3'],['2','1']],
+            labelsX: ['LabelX1','labelX2'],
+            labelsY: ['LabelY1','labelY2']
         }
+    }
+
+    addYClicked(){
+        this.setState((state) => ({ y: (state.y + 1)}));
+        this.setState((state) => ({ dataXY: state.dataXY.concat([new Array(this.state.x).fill('0'),])}));
+        this.setState((state) => ({ labelsY: state.labelsY.concat('NewYLabel')}));
+        console.log("klick_Y");
+        console.log(this.state.y);
+    }
+
+    addXClicked(){
+        this.setState((state) => ({ x: (state.x + 1)}));
+        let arr = this.state.dataXY.forEach(row => row.push('0'));
+        //this.setState((state) => ({ dataXY: arr}));
+        this.setState((state) => ({ labelsX: state.labelsX.concat('NewXLabel')}));
+        //this.setState((state) => ({ dataXY: state.dataXY.forEach(row => row.push('0'))}));
+        console.log("klick_X");
+        console.log(this.state.x);
+    }
+
+    generateGrid() {
+
+        let grid = [ [{
+            readOnly: true, component: (
+                <button onClick={() => console.log(this.state)}>
+                    Data Import/Export
+                </button>
+            ), forceComponent: true
+        }].concat(this.state.labelsX.map(val => ({value: `${val}`})),[{readOnly: true, component: (<button onClick={() => this.addXClicked()}>+</button>), forceComponent: true}])]
+        for (let i=0; i<this.state.y;i++) {
+            grid.push([{value: this.state.labelsY[i]}].concat(this.state.dataXY[i].map(val => ({value: `${val}`}))));
+        }
+        grid.push([{  readOnly: true, component:(<button onClick={() => this.addYClicked()}>+</button>), forceComponent: true}]);
+        return grid;
     }
 
     render() {
         return (
             <ReactDataSheet
-                data={this.state.grid}
+                data={this.generateGrid()}
                 valueRenderer={(cell) => cell.value}
+                dataRenderer={(cell) => cell.value}
                 onCellsChanged={changes => {
-                    const grid = this.state.grid.map(row => [...row]);
+                    const dataXY = this.state.dataXY.map(row => [...row]);
                     changes.forEach(({cell, row, col, value}) => {
-                        grid[row][col] = {...grid[row][col], value}
+                        if (row > 0 && col > 0) {
+                            dataXY[row-1][col-1] =  value;
+                        }
+                        console.log(' '+ cell + ' '+ row +' '+ col +' '+ value);
                     });
-                    this.setState({grid})
-                }}
+                    this.setState(() => ({ dataXY: dataXY}))
+                    console.log("Hello")
+                }
+                }
             />
         );
     }
