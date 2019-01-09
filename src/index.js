504ce302a7596ea40a1b5e855d7a8e146e190ce5
Patryk Cholewa <patr.cholewa@gmail.com>
2019-01-09 01:40:34 +0100

    add material-ui

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 import App from './components/App';
-import './index.css';
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
