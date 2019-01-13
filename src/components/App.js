be13b4dbdd384c653e5bad0b98d2c57c24fad837
Patryk Cholewa <patr.cholewa@gmail.com>
2019-01-13 15:02:28 +0100

    add topbar

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,32 @@ import VisibleChart from "../containers/VisibleChart";
 import VisibleUndoButton from "../containers/VisibleUndoButton";
 import VisibleRedoButton from "../containers/VisibleRedoButton";
 import VisibleSetChartTypeMenu from "../containers/VisibleSetChartTypeMenu";
+import Typography from "@material-ui/core/Typography/Typography";
+import AppBar from "@material-ui/core/AppBar/AppBar";
+import Toolbar from "@material-ui/core/Toolbar/Toolbar";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <VisibleDataSheet/>
-                <VisibleChart/>
-                <VisibleUndoButton variant="contained" color="default">UNDO</VisibleUndoButton>
-                <VisibleRedoButton variant="contained" color="default">REDO</VisibleRedoButton>
-                <VisibleFileReaderBtn/>
-                <VisibleFileDownloadLink/>
-                <VisibleSetChartTypeMenu/>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="title" color="inherit">
+                            CHART MASTER
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <aside>
+                    <VisibleUndoButton variant="contained" color="default">UNDO</VisibleUndoButton>
+                    <VisibleRedoButton variant="contained" color="default">REDO</VisibleRedoButton>
+                    <VisibleFileReaderBtn/>
+                    <VisibleFileDownloadLink/>
+                    <VisibleSetChartTypeMenu/>
+                </aside>
+                <main>
+                    <VisibleDataSheet/>
+                    <VisibleChart/>
+                </main>
             </div>
         );
     }
